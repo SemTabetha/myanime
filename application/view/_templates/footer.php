9799f7310ad8174ab7c8850342bc1e1bf7e4bc7d
Killian <ibskillianzad@gmail.com>
2016-05-31 13:53:19 +0200

    ending class for disc

--- application/view/_templates/footer.php
+++ application/view/_templates/footer.php
@@ -1,6 +1,8 @@
         <div class="footer">Footer</div>
         </div><!-- close class="wrapper" -->
-	<a href="#" class="disclaimer">Disclaimer</a>
+		<div class="ending">
+			<a href="#" class="disclaimer">Disclaimer</a>
+		</div>
 
 </body>
 </html>
