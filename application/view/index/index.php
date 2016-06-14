cfdc06e5099075cab0a7974c5a5a805b37dfad3b
SemTabetha <99035834@mydavinci.nl>
2016-06-14 11:11:53 +0200

    genre icon list
    Changed genre-icon list into real (still non functional) links
    
    Added fading hover effect

--- application/view/index/index.php
+++ application/view/index/index.php
@@ -3,16 +3,59 @@
     <div class="box" id="icons">
         <!-- echo out the system feedback (error and success messages) -->
         <?php $this->renderFeedbackMessages(); ?>
-        <img class="icon" src="http://www.semzee.dvc-icta.nl/pic/romance.png" alt="">
-        <img class="icon" src="http://www.semzee.dvc-icta.nl/pic/romance.png" alt="">
-        <img class="icon" src="http://www.semzee.dvc-icta.nl/pic/romance.png" alt="">
-        <br>
-        <img class="icon" src="http://www.semzee.dvc-icta.nl/pic/romance.png" alt="">
-        <img class="icon" src="http://www.semzee.dvc-icta.nl/pic/romance.png" alt="">
-        <br>
-        <img class="icon" src="http://www.semzee.dvc-icta.nl/pic/romance.png" alt="">
-        <img class="icon" src="http://www.semzee.dvc-icta.nl/pic/romance.png" alt="">
-        <img class="icon" src="http://www.semzee.dvc-icta.nl/pic/romance.png" alt="">
+        <ul class="icon-list">
+          <li>
+            <a href="#">
+              <img src="http://www.semzee.dvc-icta.nl/pic/romance.png" />
+              <span class="icon-content"><span>Romance</span></span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src="http://www.semzee.dvc-icta.nl/pic/romance.png" />
+              <span class="icon-content"><span>Adventure</span></span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src="http://www.semzee.dvc-icta.nl/pic/romance.png" />
+              <span class="icon-content"><span>Comedy</span></span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src="http://www.semzee.dvc-icta.nl/pic/romance.png" />
+              <span class="icon-content"><span>Fantasy</span></span>
+            </a>
+          </li>
+          </ul>
+          <ul class="icon-list">
+          <li>
+            <a href="#">
+              <img src="http://www.semzee.dvc-icta.nl/pic/romance.png" />
+              <span class="icon-content"><span>Horror</span></span>
+            </a>
+          </li>
+
+          <li>
+            <a href="#">
+              <img src="http://www.semzee.dvc-icta.nl/pic/romance.png" />
+              <span class="icon-content"><span>Ecchi</span></span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src="http://www.semzee.dvc-icta.nl/pic/romance.png" />
+              <span class="icon-content"><span>Sci-Fi</span></span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src="http://www.semzee.dvc-icta.nl/pic/romance.png" />
+              <span class="icon-content"><span>Sports</span></span>
+            </a>
+          </li>
+        </ul>
     </div>
     <div class="box" id="about-us">
         <h1>About us</h1>
